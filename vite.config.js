80be02c12dcdfa16fb0cfc74caf51ef2065f8c4c
AK <krichevtsov.aleksandr@student.ehu.lt>
2026-04-20 11:22:36 +0400

    fix: removed unnecessary files

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+    include: [
+      "src/**/*.test.ts",
+      "src/**/*.test.tsx",
+      "integration/**/*.integration.test.ts",
+    ],
   },
 });
